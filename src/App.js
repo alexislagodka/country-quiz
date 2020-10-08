4cb697f83fcb8ae6d1290d2e76416135f0febec5
Alexis LAGODKA <alexis.lagodka@bethunebruay.fr>
2020-10-08 13:44:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import quizimg from './img/quiz.svg';
+import win from './img/win.svg';
+import Question from './Question';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+  
+  state = {
+    counter: 0,
+    lose: false,
+    key:0
+  }
+  
+  handleNext = (lose) => {
+    console.log("Next");
+    console.log(lose);
+    
+      this.setState({
+        key: Math.floor(Math.random() * Math.floor(1000)),
+        lose:lose
+      })
+    
+    
+  }
+
+  render(){
+    return <div className="App">
+      <div className="main">
+        <div className ="header-card">
+          <h1>COUNTRY QUIZ</h1>
+          <img className="quiz-img" src={quizimg} alt="quizimg"></img>
+        </div>
+        <div className="quiz-card">
+          { this.state.lose ?
+          <div className="results">
+            <div><img className="results-img" src={win} alt="win"/></div>
+            <div><h3>Results</h3></div>
+            <div>You got <h4>&nbsp;{this.state.counter}&nbsp;</h4>correct answers</div>
+            <div>
+              <button 
+                className="tryAgain-button" 
+                onClick={()=> this.setState({counter: 0, lose: false, key:0})}>Try again</button>
+            </div>
+          </div>
+          :
+          <Question 
+            key={this.state.key}
+            addScore={() => (this.setState({counter: this.state.counter + 1}))}
+            handleNext={(lose)=> this.handleNext(lose)}
+            /> 
+          }
+        </div>
+      </div>
+      <div className="footer">
+        By Alexis Lagodka © 2020
+      </div>
     </div>
-  );
+  }
 }
+
 
 export default App;
---
+++ src/Question.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import check from './img/check.svg';
+import cross from './img/cross.svg';
+
+class Question extends React.Component {
+
+    region = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+    letter = ["A", "B", "C", "D"];
+    state = {
+        data : [],
+        question : "",
+        answer : "",
+        answers : [],
+        counter : 0,
+        disable : false,
+        showNext : false,
+        lose :false
+    }
+    componentWillMount() {
+        const random = Math.floor(Math.random() * this.region.length);
+        const region = this.region[random];
+        console.log(this.region[random]);
+        fetch(`https://restcountries.eu/rest/v2/region/${region}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            this.setState({
+            data : data
+            });
+            this.generateQuestion();
+        })
+        .catch(function (err) {
+        console.log('Something went wrong.', err);
+        });
+    }
+
+    generateQuestion = () => {
+        const typeOfQuestion = Math.round(Math.random()); // Output 0: capital question or 1: flag question
+        const random = Math.floor(Math.random() * this.state.data.length);
+        
+        const country = this.state.data[random].name;
+        const flag = this.state.data[random].flag;
+        const capital = this.state.data[random].capital;
+        const answer = country;
+
+        var question;
+        var answers = [country];
+
+        if (typeOfQuestion === 0){
+            question = <div className="question-container">
+                <img className="flag-img" src={flag} alt="flag"/>
+                <div className="question">Wich counry does this flag belong to ? </div>
+            </div>
+        } else {
+            question = <div className="question-container">
+                <div className="question">{capital} is the capital of ?</div>
+            </div>
+        }
+
+        for (var n=0;n<=2;n++){
+            const random = Math.floor(Math.random() * this.state.data.length);
+            const country = this.state.data[random].name;
+            console.log(`Push ${country}`);
+            answers.push(country); 
+        }
+        function shuffle(array) {
+            array.sort(() => Math.random() - 0.5);
+          }
+
+        console.log(answers);
+        shuffle(answers);
+        console.log(answers);
+        
+        this.setState({
+            question : question,
+            answer : answer,
+            answers : answers
+        });
+    }
+    
+    handleClickAnswer(answer) {
+        var crossImg = document.createElement("img");
+        crossImg.src = cross;
+        var checkImg = document.createElement("img");
+        checkImg.src = check;
+       
+        if (this.state.disable === false){
+            console.log(answer);
+            if (answer === this.state.answer) {
+                console.log("Win")
+                document.getElementById(this.state.answer).style.background = "#60BF88";
+                document.getElementById(this.state.answer).children[2].appendChild(checkImg);
+                this.props.addScore();
+            } else {
+                console.log("Lose");
+                console.log(answer);
+                document.getElementById(this.state.answer).style.background = "#60BF88";
+                document.getElementById(this.state.answer).children[2].appendChild(checkImg);
+                document.getElementById(answer).style.background = "#EA8282";
+                document.getElementById(answer).children[2].appendChild(crossImg);
+                this.setState({
+                    lose: true
+                })
+            }
+
+            this.setState({
+                disable:true,
+                showNext: true
+            })
+        }
+      }
+    render(){
+        
+        return <div className="question-component"> 
+            {this.state.question}
+            
+            <div className="answer-container" id="answer-container">
+            {this.state.answers.map( (answer, index) => (
+                    <button disabled={this.state.disable} key={index}  className="answer-button" id={answer} value={answer} onClick={() => this.handleClickAnswer(answer)}>
+                                <div value={answer}>{this.letter[index]}</div>
+                                <div value={answer}>{answer}</div>
+                                <div value={answer} className="icon"></div>
+                    </button>
+            ))}
+            </div>
+            <div className="next-button-container">
+                {this.state.showNext ? <button className="next-button" onClick={() => this.props.handleNext(this.state.lose)}>Next</button> : ""}
+            </div>
+        </div>
+    }
+}
+export default Question
